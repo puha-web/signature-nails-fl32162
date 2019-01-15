35e55139c60c172d1ad6d593384fa97b78f501ba
Phung Pham <phung.tech@gmail.com>
2019-01-14 23:12:50 -0500

    updated react theme

---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+
+var indexRoutes = [
+  { path: "/", name: "LandingPage", component: LandingPage },
+
+];
+
+export default indexRoutes;
