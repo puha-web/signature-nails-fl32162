3ccd77e73ae1b4aea277c7f42e53bd40218d3f21
Phung Pham <phung.tech@gmail.com>
2019-02-02 18:34:02 -0500

    updated social links

--- src/views/Components/SocialMedia.jsx
+++ src/views/Components/SocialMedia.jsx
@@ -7,6 +7,9 @@ import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 
+//import data
+import SocialLinks from '../../db/Social';
+
 
 
 
@@ -22,7 +25,7 @@ class Media extends React.Component {
             <h4 className={classes.title}>Thank you for visiting us!</h4>
 
             <Button round color="google" style={{ width: '150px'}}
-              href="https://www.google.com/maps/place/Signature+Nails/@28.9083275,-81.9716632,15z/data=!4m5!3m4!1s0x0:0xb2bcce2499f299cb!8m2!3d28.9083275!4d-81.9716632"
+              href={SocialLinks.google} 
               target="_blank"
             >
               <i
@@ -31,7 +34,7 @@ class Media extends React.Component {
               Google
             </Button>
             <Button round color="facebook" style={{ width: '150px'}}
-              href="https://www.facebook.com/LCN-Nail-Spa-1583160825231938/"
+              href={SocialLinks.facebook}
               target="_blank"
             >
               <i
@@ -40,7 +43,7 @@ class Media extends React.Component {
               FaceBook
             </Button>
             <Button round style={{backgroundColor: '#FCF3CF', color: 'black', width: '150px'}}
-              href="https://www.yelp.com/biz/lcn-nail-spa-raleigh"
+              href={SocialLinks.yelp}
               target="_blank"
             >
               <i
